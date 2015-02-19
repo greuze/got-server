0291e3feac852a550e3312cc3be61cb422de11f9
Iván López <ils@tid.es>
2015-02-19 10:05:53 +0100

    Implemented place orders and added some tests more

--- test/unit/service/action-validator_test.js
+++ test/unit/service/action-validator_test.js
@@ -5,18 +5,15 @@ var chai = require ('chai');
 /*jshint loopfunc:true */
 describe('Action validator (3 players)', function() {
 
-    var game, storage, validator;
+    var game, storage, action, validator;
     var p1, p2, p3;
-    var consoleLog;
 
     // Must create a full game, to let other tests run properly
     before(function(done) {
         game = require('../../../lib/service/game-service.js');
         storage = require('../../../lib/service/game-storage.js');
+        action = require('../../../lib/service/action.js');
         validator = require('../../../lib/service/action-validator.js');
-        // Disable console.log
-        consoleLog = console.log;
-        console.log = function() {};
 
         game.createGame({maxPlayers: 3}, function(error, player) {
             p1 = player;
@@ -28,12 +25,6 @@ describe('Action validator (3 players)', function() {
                 });
             });
         });
-    });
-
-    after(function(done) {
-        console.log = consoleLog;
-
-        done();
     });
 
     function getPlayerIdByHouse(house) {
@@ -215,6 +206,75 @@ describe('Action validator (3 players)', function() {
 
         var starkId = getPlayerIdByHouse('stark');
         chai.assert.equal(validator.canPlaceOrders(myGame, starkId, orders), false,
+            'Orders validation are not correct');
+
+        done();
+    });
+
+    it('Zone belongs to other player (Baratheon)', function(done) {
+        var myGame = storage.getRunningGame(p1.gameId);
+        var orders = [
+            {
+                zone: 'winterfell',
+                order: 'MARCH*'
+            },
+            {
+                zone: 'dragonstone',
+                order: 'RAID'
+            }
+        ];
+
+        var starkId = getPlayerIdByHouse('baratheon');
+        chai.assert.equal(validator.canPlaceOrders(myGame, starkId, orders), false,
+            'Orders validation are not correct');
+
+        done();
+    });
+
+    it('Can place less orders than available (Baratheon)', function(done) {
+        var myGame = storage.getRunningGame(p1.gameId);
+        var orders = [
+            {
+                zone: 'shipbreaker-bay',
+                order: 'MARCH*'
+            },
+            {
+                zone: 'dragonstone',
+                order: 'RAID'
+            }
+        ];
+
+        var starkId = getPlayerIdByHouse('baratheon');
+        chai.assert.equal(validator.canPlaceOrders(myGame, starkId, orders), true,
+            'Orders validation are not correct');
+
+        done();
+    });
+
+    it('Cannot place orders twice (Stark)', function(done) {
+        var myGame = storage.getRunningGame(p1.gameId);
+        var starkId = getPlayerIdByHouse('stark');
+
+        var orders1 = [
+            {
+                zone: 'winterfell',
+                order: 'CROWN*'
+            }
+        ];
+        var orders2 = [
+            {
+                zone: 'white-harbor',
+                order: 'MARCH'
+            }
+        ];
+
+        chai.assert.equal(validator.canPlaceOrders(myGame, starkId, orders1), true,
+            'Orders validation are not correct');
+        chai.assert.equal(validator.canPlaceOrders(myGame, starkId, orders2), true,
+            'Orders validation are not correct');
+        // After placing orders once, no more orders placement is valid for that player
+        action.placeOrders(myGame, starkId, orders1);
+        chai.assert.equal(validator.canPlaceOrders(myGame, starkId, orders2), false,
             'Orders validation are not correct');
 
         done();
